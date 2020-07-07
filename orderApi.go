5deb4abbd19081cc5e299ec20c02835d0e6fe193
alex-orkuma <orkumashi@gmail.com>
2020-07-07 01:12:20 +0100

    Added documentation to the api using swaggo

--- orderApi.go
+++ orderApi.go
@@ -8,23 +8,37 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
+// Order represents the model for an order
 type Order struct {
-	OrderId      string    `json:"orderId"`
-	CustomerName string    `json:"customerName"`
-	OrderedAt    time.Time `json:orderedAt`
-	Items        []Item    `json:items`
+	OrderId      string    `json:"orderId" example:"1"`
+	CustomerName string    `json:"customerName" example:"Alex Orkuma"`
+	OrderedAt    time.Time `json:"orderedAt" example:"2019-11-09T21:21:46+00:00"`
+	Items        []Item    `json:"items"`
 }
 
+// Item represents the model for an item in the order
 type Item struct {
-	ItemId      string `json:"itemId"`
-	Description string `json:"description"`
-	Quantity    int    `json:"quantity"`
+	ItemId      string `json:"itemId" example:"A1B2C3"`
+	Description string `json:"description" example:"A random description"`
+	Quantity    int    `json:"quantity" example:"1"`
 }
 
 var orders []Order
 var prevOrderId = 0
+
+// @title Orders API
+// @version 1.0
+// @description This is a sample serice for managing orders
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email soberkoder@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8080
+// @BasePath /
 
 func main() {
 	router := mux.NewRouter()
@@ -44,6 +58,15 @@ func main() {
 	log.Fatal(http.ListenAndServe(":9090", router))
 }
 
+// CreateOrder godoc
+// @Summary Create a new order
+// @Description Create a new order with the input paylod
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param order body Order true "Create order"
+// @Success 200 {object} Order
+// @Router /orders [post]
 func createOder(w http.ResponseWriter, r *http.Request) {
 	var order Order
 	json.NewDecoder(r.Body).Decode(&order)
@@ -54,6 +77,14 @@ func createOder(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(order)
 }
 
+// GetOrders godoc
+// @Summary Get details of all orders
+// @Description Get details of all orders
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} Order
+// @Router /orders [get]
 func getOrders(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(orders)
